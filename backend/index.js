059246785c3ee689fb9dd9f938e5d5cf20004b3e
Lukas Mathae <mathae@student.tugraz.at>
2024-11-13 09:31:47 +0900

    Add limiter

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,15 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
 // Middleware
 app.use(cors());
 app.use(express.json());
