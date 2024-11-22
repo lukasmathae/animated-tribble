f8fb4c79592a8d048df82ff128e3d35e8def0e35
Lukas Mathae <mathae@student.tugraz.at>
2024-11-22 16:45:59 +0900

    Add loading animation to NewsSingleArticle.js

--- frontend/src/components/NewsSingleArticle.js
+++ frontend/src/components/NewsSingleArticle.js
@@ -22,7 +22,10 @@ const ArticleDetail = () => {
     }, [id]);
 
     if (!article) {
-        return <div>Loading...</div>;
+        return <div className="flex justify-center items-center h-64">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-600"></div>
+            <span className="ml-4 text-lg text-blue-600">Loading article...</span>
+        </div>;
     }
 
     return (
